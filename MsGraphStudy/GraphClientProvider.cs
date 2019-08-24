c1492c794981e77b8b02698ca91293f0b6dd9a4e
TKS0522 <t_o_19880522@outlook.com>
2019-08-24 12:09:21 +0900

    init

---
+++ MsGraphStudy/GraphClientProvider.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsGraphStudy
+{
+    public static class GraphClientProvider
+    {
+        private static IConfidentialClientApplication confidentialClientApp;
+        private static IPublicClientApplication publicClientApp;
+
+        private static GraphServiceClient graphClient;
+
+        private static AuthenticationResult authentication;
+
+        public static void MakeConfidtentionalClient(string clientId, string tenantId, string clientSecret)
+        {
+            confidentialClientApp = ConfidentialClientApplicationBuilder.Create(clientId)
+                    .WithAuthority("https://login.microsoftonline.com/" + tenantId)
+                    .WithClientSecret(clientSecret)
+                    .Build();
+        }
+
+        public static void MakePublicClient(string clientId, string tenantId)
+        {
+            publicClientApp = PublicClientApplicationBuilder.Create(clientId)
+                .WithAuthority("https://login.microsoftonline.com/" + tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+        }
+
+        public static async Task<string> GetTokenForConfidentialClientAsync(IEnumerable<string> scopes)
+        {
+            try
+            {
+                authentication = await confidentialClientApp.AcquireTokenForClient(scopes).ExecuteAsync();
+                return authentication.AccessToken;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static  async Task<string> GetTokenForPublicClientAsync(IEnumerable<string> scopes)
+        {
+            try
+            {
+                authentication = await publicClientApp.AcquireTokenInteractive(scopes).ExecuteAsync();
+                return authentication.AccessToken;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static GraphServiceClient GetGraphServiceClient(IEnumerable<string> scopes)
+        {
+            if (graphClient == null)
+            {
+                try
+                {
+                    graphClient = new GraphServiceClient(
+                        new DelegateAuthenticationProvider(
+                            async (requestMessage) => {
+                                var token = authentication.AccessToken;
+                                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+                            })
+                        );
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            return graphClient;
+        }
+    }
+}
---
+++ MsGraphStudy/Program.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Graph;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace MsGraphStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("using Secret(y/n)?");
+            var usingSecret = (Console.ReadLine() == "y");
+
+            Console.WriteLine("Enter ClientId");
+            var clientId = Console.ReadLine();
+
+            Console.WriteLine("Enter TenantId");
+            var tenantId = Console.ReadLine();
+
+            var secret = "";
+            if (usingSecret)
+            {
+                Console.WriteLine("Enter Secret");
+                secret = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter Scopes(exit='fin')");
+            var isfin = false;
+            var scopes = new List<string>();
+            do
+            {
+                var scope = Console.ReadLine();
+                isfin = (scope == "fin");
+                if (!isfin)
+                {
+                    scopes.Add(scope);
+                }
+            } while (!isfin);
+
+            Console.WriteLine("GetToken!!---------------------------------------------");
+            if (usingSecret)
+            {
+                GraphClientProvider.MakeConfidtentionalClient(clientId, tenantId, secret);
+                var res = GraphClientProvider.GetTokenForConfidentialClientAsync(scopes).Result;
+                Console.WriteLine(res);
+            }
+            else
+            {
+                GraphClientProvider.MakePublicClient(clientId, tenantId);
+                var res = GraphClientProvider.GetTokenForPublicClientAsync(scopes).Result;
+                Console.WriteLine(res);
+            }
+
+            Console.WriteLine("AccessGraph--------------------------------------------");
+            var client = GraphClientProvider.GetGraphServiceClient(scopes);
+
+            Console.WriteLine("----User(Me)-----------------------------------------------");
+            try
+            {
+                var data = client.Me.Request().GetAsync().Result;
+                var json = JsonConvert.SerializeObject(data);
+                Console.WriteLine(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception------------------------------------------");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("----User(PrincipalName)-----------------------------------------------");
+            try
+            {
+                var name = Console.ReadLine();
+                var data = client.Users[name].Request().GetAsync().Result;
+                var json = JsonConvert.SerializeObject(data);
+                Console.WriteLine(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception------------------------------------------");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("----CalenderView(Me)-----------------------------------------------");
+            try
+            {
+                var options = new List<Option>() {
+                    new QueryOption("startDateTime", new DateTime(2019, 1, 1).ToString("yyyy-MM-ddThh:mm:ss")),
+                    new QueryOption("endDateTime", new DateTime(2019, 12, 31).ToString("yyyy-MM-ddThh:mm:ss"))
+                };
+                var data = client.Me.CalendarView.Request(options).GetAsync().Result;
+                var json = JsonConvert.SerializeObject(data);
+                Console.WriteLine(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception------------------------------------------");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("----CalenderView(User)-----------------------------------------------");
+            try
+            {
+                var options = new List<Option>() {
+                    new QueryOption("startDateTime", new DateTime(2019, 1, 1).ToString("yyyy-MM-ddThh:mm:ss")),
+                    new QueryOption("endDateTime", new DateTime(2019, 12, 31).ToString("yyyy-MM-ddThh:mm:ss"))
+                };
+                var name = Console.ReadLine();
+                var data = client.Users[name].CalendarView.Request(options).GetAsync().Result;
+                var json = JsonConvert.SerializeObject(data);
+                Console.WriteLine(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception------------------------------------------");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("----Mail(Me)-----------------------------------------------");
+            try
+            {
+                var data = client.Me.MailFolders.Request().GetAsync().Result;
+                var json = JsonConvert.SerializeObject(data);
+                Console.WriteLine(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception------------------------------------------");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("----Mail(User)-----------------------------------------------");
+            try
+            {
+                var name = Console.ReadLine();
+                var data = client.Users[name].MailFolders.Request().GetAsync().Result;
+                var json = JsonConvert.SerializeObject(data);
+                Console.WriteLine(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception------------------------------------------");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
